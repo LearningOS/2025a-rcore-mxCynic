af63f82ac326c8cd2e4978a2f367997fcdac10ec
mxCynic <mx1545914198@gmail.com>
2025-09-25 16:44:43 +0800

    update ch3

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -27,8 +27,12 @@ mod process;
 use fs::*;
 use process::*;
 
+static mut SYSCALL_CAL: [isize; 500] = [0; 500];
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
+    unsafe {
+        SYSCALL_CAL[syscall_id] += 1;
+    }
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,6 @@
 //! Process management syscalls
 use crate::{
+    syscall::SYSCALL_CAL,
     task::{exit_current_and_run_next, suspend_current_and_run_next},
     timer::get_time_us,
 };
@@ -39,7 +40,18 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 }
 
 // TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
+    match trace_request {
+        0 => (id as *const u8) as isize,
+
+        1 => {
+            unsafe {
+                let id_ptr = id as *mut u8;
+                *id_ptr = data as u8;
+            }
+            0
+        }
+        2 => unsafe { SYSCALL_CAL[id] },
+        _ => -1,
+    }
 }
