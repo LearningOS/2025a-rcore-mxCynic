6e3ca15571968f0a892975fd6e14c01ce6fbd57a
mxCynic <mx1545914198@gmail.com>
2025-09-29 16:05:05 +0800

    add: finish get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,13 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+
+use crate::{
+    mm::translated_byte_buffer,
+    task::{
+        change_program_brk, current_user_token, exit_current_and_run_next,
+        suspend_current_and_run_next,
+    },
+    timer::get_time,
+};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -25,15 +33,39 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!("kernel: sys_get_time");
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    // trace!("kernel: sys_get_time");
+    let us = get_time();
+    let time = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let len = core::mem::size_of::<TimeVal>();
+    let buffers = translated_byte_buffer(current_user_token(), ts as *const u8, len);
+    let src = unsafe { core::slice::from_raw_parts(&time as *const TimeVal as *const u8, len) };
+    let buffers_len = buffers.concat().len();
+    let src_len = src.len();
+
+    let mut src_iter = src.iter();
+    for buf in buffers.into_iter() {
+        for b in buf.iter_mut() {
+            if let Some(s) = src_iter.next() {
+                *b = *s;
+            }
+        }
+    }
+    if buffers_len == src_len {
+        0
+    } else {
+        -1
+    }
 }
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
+    // trace!("kernel: sys_trace");
+
     -1
 }
 
